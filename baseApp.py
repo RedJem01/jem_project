7337d0a1be3828ff43e7e6f625b8f95c072da832
Jem <79197456+RedJem01@users.noreply.github.com>
2024-02-14 17:05:12 +0000

    Add use of model and dialogs for if correct image or not

--- baseApp.py
+++ baseApp.py
@@ -1,6 +1,11 @@
 import time
 import os
 import glob
+import numpy as np
+
+from keras.saving import load_model
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.resnet50 import preprocess_input
 
 from kivymd.app import MDApp
 from kivymd.uix.menu import MDDropdownMenu
@@ -10,18 +15,30 @@ from kivymd.uix.card import  MDCard
 from kivymd.uix.label import MDLabel
 from kivymd.uix.anchorlayout import AnchorLayout
 from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton, MDRaisedButton, MDIconButton
+from kivymd.uix.button import MDFlatButton, MDIconButton
+from kivy.uix.image import Image
 
+#Needed for MDDropdownMenu
 class IconListItem(OneLineIconListItem):
     icon = StringProperty()
 
 class MainApp(MDApp):
-    dialog = None
+    sameTaskDialog = None
+    doneTaskDialog = None
+    failedTaskDialog = None
+    classes = ["brushing_teeth", "cutting_nails", "doing_laundry", "folding_clothes", "washing_dishes"]
+    currentTaskString = None
+    currentTaskCard = None
 
     def build(self):
+        #Set colours for app
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Red"
+        
+        #Set tasks items for drop down menu
         taskItems = [{"viewclass": "IconListItem", "text": "Brushing teeth", "on_release": lambda x="Brushing teeth": self.setItem(x)}, {"viewclass": "IconListItem", "text": "Cutting nails", "on_release": lambda x="Cutting nails": self.setItem(x)}, {"viewclass": "IconListItem", "text": "Doing laundry", "on_release": lambda x="Doing laundry": self.setItem(x)}, {"viewclass": "IconListItem", "text": "Folding clothes", "on_release": lambda x="Folding clothes": self.setItem(x)}, {"viewclass": "IconListItem", "text": "Washing dishes", "on_release": lambda x="Washing dishes": self.setItem(x)}]
+        
+        #Make drop down menu
         self.menu = MDDropdownMenu(
             caller=self.root.ids.taskDropDown,
             items=taskItems,
@@ -29,74 +46,170 @@ class MainApp(MDApp):
         )
         self.menu.bind()
 
+
+
     def toTaskScreen(self):
         self.root.current = "addTaskScreen"
 
     def toHomeScreen(self):
-        self.root.current = "homeScreen"
+        #Delete all images taken so far
         files = glob.glob('.\\takenImages\\*')
         for f in files:
             os.remove(f)
+        self.currentTaskCard = None
+        self.currentTaskString = None
+        self.root.current = "homeScreen"
 
+    def toCameraScreen(self):
+        self.root.current = "cameraScreen"
+
+    def toImageScreen(self, fileName):
+        for child in self.root.ids.imageLayout.children:
+            if (type(child) == Image):
+                self.root.ids.imageLayout.remove_widget(child)
+        #Add image to screen after capturing from camera
+        image = Image(source=".\\takenImages\\{}".format(fileName))
+        self.root.ids.imageLayout.add_widget(image)
+        self.root.current = "imageScreen"
+
+
+
+    #Sets the current item in the task drop down menu
     def setItem(self, textItem):
         self.root.ids.taskDropDown.set_item(textItem)
         self.menu.dismiss()
 
     def setTask(self):
+        #String of current item in drop down menu
         taskString = self.root.ids.taskDropDown.current_item
-        print("Setting task")
-        print(type(self.root.ids.homeScreenWidgetLayout.children[0]))
+        #Check if no tasks anchor layout is still on the screen and remove if it is
         if (type(self.root.ids.homeScreenWidgetLayout.children[0]) == AnchorLayout):
             self.root.ids.homeScreenWidgetLayout.remove_widget(self.root.ids.homeScreenWidgetLayout.children[0])
+        
+        #Make id for task card based on task string e.g. card_Brushing_teeth
         splitTask = taskString.split()
         cardId = "card_" + splitTask[0] + "_" + splitTask[1]
+        cameraButtonId = "button_" + splitTask[0] + "_" + splitTask[1]
+        
+        #Loop through children in home screen layout and check if task already exists
         for child in self.root.ids.homeScreenWidgetLayout.children:
-            if (type(child) == AnchorLayout):
-                continue
+            #If task exists then open a dialog box that says it already exists
             if (child.id == cardId):
-                self.openDialog()
+                self.openSameTaskDialog()
                 return
 
+        #Add camera button in the left center of the card
         cameraAnchor = AnchorLayout(anchor_x="left", anchor_y="center")
-        cameraButton = MDIconButton(icon="camera", on_release=self.takeImage)
+        cameraButton = MDIconButton(id=cameraButtonId, icon="camera", on_release=self.setCurrentTask)
         cameraAnchor.add_widget(cameraButton)
 
+        #Add a delete button in the right center of the card
         deleteAnchor = AnchorLayout(anchor_x="right", anchor_y="center")
         deleteButton = MDIconButton(icon="delete", on_release=self.deleteTask)
         deleteAnchor.add_widget(deleteButton)
 
+        #Add task label to the left top of the card
         labelAnchor = AnchorLayout(anchor_x="left", anchor_y="top")
         cardLabel = MDLabel(text=taskString, height=50)
         labelAnchor.add_widget(cardLabel)
 
+        #Add card with above widgets ^^^^
         self.root.ids.homeScreenWidgetLayout.add_widget(MDCard(labelAnchor, cameraAnchor, deleteAnchor, id=cardId, padding=20))
         self.toHomeScreen()
-
-    def openDialog(self):
-        if not self.dialog:
-            self.dialog = MDDialog(text="That task is already set. Please pick another one or return to the home screen.", buttons=[MDFlatButton(text="OK", on_release=self.closeDialog)])
-        self.dialog.open()
-
-    def closeDialog(self, ah):
-        self.dialog.dismiss(force=True)
+        
+    def setCurrentTask(self, instance):
+        buttonId = instance.id
+        splitId = buttonId.split("_")
+        self.currentTaskString = splitId[1] + "_" + splitId[2]
+        self.currentTaskCard = instance.parent.parent
+        self.toCameraScreen()
 
     def deleteTask(self, instance):
+        #Delete card (instance is the delete button on card to delete, parent is layout parent.parent is card)
         self.root.ids.homeScreenWidgetLayout.remove_widget(instance.parent.parent)
+        #Check if any widgets exist in homeScreenWidgetLayout (check if no tasks)
         if (len(self.root.ids.homeScreenWidgetLayout.children) == 0):
+            #Add no tasks message to home screen
             anchor =  AnchorLayout(anchor_x="center", anchor_y="center")
             label = MDLabel(id="noTasksYet", text="You have no tasks yet. Click the plus icon in the top right to add a task :)", adaptive_size=True, bold=True)
             anchor.add_widget(label)
             self.root.ids.homeScreenWidgetLayout.add_widget(anchor)
-            
-    def takeImage(self, ah):
-        self.root.current = "cameraScreen"
-    
+
+
+
+    def openSameTaskDialog(self):
+        #Check if dialog exists
+        if not self.sameTaskDialog:
+            #Make dialog with warning and ok button that closes dialog
+            self.sameTaskDialog = MDDialog(text="That task is already set. Please pick another one or return to the home screen.", buttons=[MDFlatButton(text="OK", on_release=self.closeSameTaskDialog)])
+        #Open dialog
+        self.sameTaskDialog.open()
+
+    def closeSameTaskDialog(self, ah):
+        self.sameTaskDialog.dismiss(force=True)
+        
+    def openDoneTaskDialog(self):
+        #Check if dialog exists
+        if not self.doneTaskDialog:
+            #Make dialog with warning and ok button that closes dialog
+            self.doneTaskDialog = MDDialog(text="Task completed :).", buttons=[MDFlatButton(text="OK", on_release=self.closeDoneTaskDialog)])
+        #Open dialog
+        self.doneTaskDialog.open()
+
+    def closeDoneTaskDialog(self, ah):
+        self.doneTaskDialog.dismiss(force=True)
+        self.toHomeScreen()
+
+    def openFailedTaskDialog(self):
+        #Check if dialog exists
+        if not self.failedTaskDialog:
+            #Make dialog with warning and ok button that closes dialog
+            self.failedTaskDialog = MDDialog(text="Task not completed, please take a picture of you doing the task or go back to home screen.", buttons=[MDFlatButton(text="OK", on_release=self.closeFailedTaskDialog)])
+        #Open dialog
+        self.failedTaskDialog.open()
+
+    def closeFailedTaskDialog(self, ah):
+        self.failedTaskDialog.dismiss(force=True)
+        self.toCameraScreen()
+
+
+
+
     def capture(self):
+        #Get camera object
         camera = self.root.ids.camera
+        #Get current time
         timeStr = time.strftime("%Y%m%d_%H%M%S")
+        #Set file name with current time
         fileName = "IMG_{}.png".format(timeStr)
+        #Save image to takenImages folder
         camera.export_to_png(".\\takenImages\\{}".format(fileName))
-        print("Captured")
+        #Go to image screen
+        self.toImageScreen(fileName)
+
+    def acceptImage(self):
+        model = load_model("./model.h5")
+        files = glob.glob('.\\takenImages\\*')
+        kerasImage = load_img(files[0])
+        x = img_to_array(kerasImage)
+        x = preprocess_input(x)
+        x = np.expand_dims(x, axis=0)
+        pred = model.predict(x)[0]
+        maxPosition=np.argmax(pred) 
+        predClass = self.classes[maxPosition]
+        if (predClass == self.currentTaskString.lower()):
+            self.root.ids.homeScreenWidgetLayout.remove_widget(self.currentTaskCard)
+            self.openDoneTaskDialog()
+        else:
+            self.openFailedTaskDialog
+
+    def deleteImage(self):
+        #Delete image from folder 
+        files = glob.glob('.\\takenImages\\*')
+        for f in files:
+            os.remove(f)
+        #Take back to camera screen
+        self.toCameraScreen(self.root.ids.deleteImageButton)
 
 if __name__ == '__main__':
     app = MainApp()
--- resnet50Code.py
+++ resnet50Code.py
@@ -13,24 +13,23 @@ IMAGE_SIZE = (150, 150)
 BATCH_SIZE = 8
 FREEZE_LAYERS = 2
 NUM_EPOCHS = 10
-WEIGHTS_FINAL = 'modelresnet50.h5'
 
 #Paths to folders holding images
-TRAIN_DATASET_PATH = './split_project_dataset/train'
-VAL_DATASET_PATH = './split_project_dataset/validation'
+TRAIN_DATASET_PATH = "./split_project_dataset/train"
+VAL_DATASET_PATH = "./split_project_dataset/validation"
 
 #Load images into tensorflow Datasets
 #ImageDataGenerator is deprecated so using image_dataset_from_directory as it says to in the tensorflow docs https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator https://www.tensorflow.org/api_docs/python/tf/keras/utils/image_dataset_from_directory
-trainDataset = image_dataset_from_directory(TRAIN_DATASET_PATH, labels='inferred', label_mode="categorical", batch_size=BATCH_SIZE, image_size=IMAGE_SIZE, interpolation="bicubic")
+trainDataset = image_dataset_from_directory(TRAIN_DATASET_PATH, labels="inferred", label_mode="categorical", batch_size=BATCH_SIZE, image_size=IMAGE_SIZE, interpolation="bicubic")
 # testDataset = image_dataset_from_directory(TEST_DATASET_PATH, labels='inferred', label_mode="categorical", shuffle=False, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE, interpolation="bicubic")
-valDataset = image_dataset_from_directory(VAL_DATASET_PATH, labels='inferred', label_mode="categorical", shuffle=False, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE, interpolation="bicubic")
+valDataset = image_dataset_from_directory(VAL_DATASET_PATH, labels="inferred", label_mode="categorical", shuffle=False, batch_size=BATCH_SIZE, image_size=IMAGE_SIZE, interpolation="bicubic")
 
 #Set up model
-resNet = ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
+resNet = ResNet50(include_top=False, weights="imagenet", input_tensor=None, input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
 x = resNet.output
 x = Flatten()(x)
 x = Dropout(0.5)(x)
-output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+output_layer = Dense(NUM_CLASSES, activation="softmax", name="softmax")(x)
 model = Model(inputs=resNet.input, outputs=output_layer)
 for layer in model.layers[:FREEZE_LAYERS]:
     layer.trainable = False
@@ -39,13 +38,13 @@ for layer in model.layers[FREEZE_LAYERS:]:
 
 model.summary()
 
-model.compile(optimizer=Adam(learning_rate=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=1e-5), loss="categorical_crossentropy", metrics=["accuracy"])
 
 model.fit(trainDataset, validation_data = valDataset, epochs = NUM_EPOCHS)
 
 #Print out metrics
 scores = model.evaluate(valDataset, verbose=1)
-print('Accuracy is %s' %(scores[1]*100))
+print("Accuracy is %s" %(scores[1]*100))
 
 Y_pred = model.predict(valDataset)
 y_pred = np.argmax(Y_pred, axis=1)
@@ -58,4 +57,4 @@ print(metrics.confusion_matrix(y_true, y_pred))
 print("Classification report")
 print(metrics.classification_report(y_true, y_pred))
 
-model.save_weights(WEIGHTS_FINAL)
+model.save("model.h5")
